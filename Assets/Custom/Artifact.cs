2602a6dd52f36ab5ea1d2c1aa463f0e76dead165
raahul1705 <raahul1705@gmail.com>
2019-02-23 10:25:05 -0600

    Setup basic UI prototype
    Added prototype UI to test scene
    - Not tested yet on VR

---
+++ Assets/Custom/Artifact.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Artifact : VRTK_InteractableObject
+{
+    [SerializeField] CanvasGroup infoBox;
+    public override void StartUsing(VRTK_InteractUse currentUsingObject = null)
+    {
+        print("Hit");
+        base.StartUsing(currentUsingObject);
+        infoBox.alpha = 1;
+    }
+
+    public override void StopUsing(VRTK_InteractUse previousUsingObject = null, bool resetUsingObjectState = true)
+    {
+        base.StopUsing(previousUsingObject, resetUsingObjectState);
+        infoBox.alpha = 0;
+    }
+
+    protected void Start()
+    {
+        infoBox = GetComponentInChildren<CanvasGroup>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+}
