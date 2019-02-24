be379b80a3b38d31e582ec0f3597462aee4f03a8
raahul1705 <raahul1705@gmail.com>
2019-02-24 06:06:24 -0600

    Edited scenes

---
+++ Assets/Custom/Scripts/Loading.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Loading : MonoBehaviour
+{
+
+    public Transform loadingBar;
+    public TextMeshProUGUI loadingText;
+    public bool readyToLoad;
+    [SerializeField] private float currentAmount;
+    [SerializeField] private float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentAmount < 100)
+        {
+            currentAmount += speed * Time.deltaTime;
+            loadingText.text = ((int)currentAmount).ToString() + "%";
+            readyToLoad = false;
+        }
+
+        else
+        {
+            readyToLoad = true;
+        }
+
+        loadingBar.GetComponent<Image>().fillAmount = currentAmount / 100;
+    }
+
+    public void ResetAmount()
+    {
+        currentAmount = 0;
+    }
+}
